1962b65d7db668cf1a0244d3b88fe9ab3cef7d9b
Iman-Usman <93043426+Iman-Usman@users.noreply.github.com>
2022-02-18 15:44:27 +0400

    Done

---
+++ products.js
@@ -0,0 +1,49 @@
+let products=[
+    {
+    "id":1001,
+    "title":"Cat Food, 25lb bag",
+    "description": "A 25 pound bag of <em>irrestible</em> organic goodness for your cat.",
+    "price": 2000,
+    "image": "images/catfood.jpg",
+    "availableInventory": 10,
+    "rating": 1
+},
+{
+    "id":1002,
+    "title":"Yarn",
+    "description": "Yarn, your cat play with for a very long time.",
+    "price": 600,
+    "image": "images/yarn.jpg",
+    "availableInventory": 6,
+    "rating": 2
+},
+{
+    "id":1003,
+    "title":"Kitty Litter",
+    "description": "Premium kitty litter for your cat.",
+    "price": 400,
+    "image": "images/kittylitter.jpg",
+    "availableInventory": 5,
+    "rating": 3
+},
+{
+    "id":1004,
+    "title":"Cat House",
+    "description": "A place for your cat to play.",
+    "price": 1000,
+    "image": "images/cathouse.jpg",
+    "availableInventory": 3,
+    "rating": 4
+},
+{
+    "id":1005,
+    "title":"Laser Pointer",
+    "description": "Drive your cat crazy with this amazing product.",
+    "price": 200,
+    "image": "images/laserpointer.jpg",
+    "availableInventory": 2,
+    "rating": 5
+},
+
+]
+
